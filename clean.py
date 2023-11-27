4760f853386c5d986adeab79fa4b10dca010a30e
wycho <wooyoung510@gmail.com>
2023-11-27 16:00:37 +0900

    file upload

--- clean.py
+++ clean.py
@@ -7,14 +7,16 @@ def get_summary(resdir, outfile):
     pbpcode = res["PBP_Code(1A:2B:2X)"].to_list()[0].split(":")
     with open(os.path.join(resdir, "BLACTAM_MIC_RF_with_SIR.txt")) as res:
         blcode = list(res.readlines())[1].split(" ")
+    header = ">PBP_Category\tType\n"
     pbp_resstring = f"1A\t{pbpcode[0]}\n2B\t{pbpcode[1]}\n2X\t{pbpcode[2]}\n"
+    pbp_header = ">Agent\tPredicted_MIC\tInferred_phenotype(non/meningitis)\n"
     AMO_resstring = f"Amoxicilin\t{blcode[4]}{blcode[5]}\t{blcode[6]}\n"
     CFT_resstring = f"Ceftriaxone\t{blcode[14]}{blcode[15]}\t{blcode[16]}\\{blcode[17]}\n"
     CFX_resscring = f"Cefuroxime\t{blcode[18]}{blcode[19]}\t{blcode[20]}\n"
     MER_resstring = f"Meropenem\t{blcode[7]}{blcode[8]}\t{blcode[9]}\n"
     PEN_resstring = f"Penicililn\t{blcode[0]}{blcode[1]}\t{blcode[2]}\\{blcode[3]}\n"
     TAX_resstring = f"Cefotaxime\t{blcode[10]}{blcode[11]}\t{blcode[12]}\\{blcode[13]}\n"
-    result = pbp_resstring + AMO_resstring + CFT_resstring + TAX_resstring + CFX_resscring + MER_resstring + PEN_resstring
+    result = header + pbp_resstring + pbp_header + AMO_resstring + CFT_resstring + TAX_resstring + CFX_resscring + MER_resstring + PEN_resstring
     with open(outfile, "w") as out:
         out.write(result)
 
